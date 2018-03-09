8e8aa58172e94e2e7e09590743710feb7c48046c
Clare Nolan <117344143@umail.ucc.ie>
2018-03-09 09:47:37 +0000

    adjustments

--- Ebus3.php
+++ Ebus3.php
@@ -32,29 +32,26 @@ session_start()
         <h4 class="ebus3_heading">Purchase Receipt</h4>
         
         <?php  
-        // Set session variables
+        
           $_SESSION["user_name"] = $_POST["user_name"];
           $_SESSION["user_email"] = $_POST["user_email"];
         ?>
         
         <p class="ebus3_name">
             <?php
-            // Echo session variables that were set on the previous pages
             echo "Name: " . $_SESSION["user_name"] . ".";
             ?>
         </p>
         
         <p class="ebus3_email">
             <?php
-            // Echo session variables that were set on the previous pages
             echo "Email: " . $_SESSION["user_email"] . ".";
             ?>
         </p>
         
         <p class="ebus3_total">
             <?php
-            // Echo session variables that were set on the previous pages
-            echo "Total: " . $_SESSION["total"] . ".";
+           echo "Total: " . $_SESSION["total"] . ".";
             ?>
         </p>
     </body>
